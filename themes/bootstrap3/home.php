3c149da16e94e1a0bf71dbaff3d16310863f9fa1
Arthur Wiebe <arthur@cirruslab.com>
2013-05-09 19:24:26 -0400

    adding jumbotron for home template

--- themes/bootstrap3/home.php
+++ themes/bootstrap3/home.php
@@ -3,9 +3,11 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php');
 ?>
 
+<div class="jumbotron">
 <?php
     $a = new Area('Header'); $a->display($c);
 ?>
+</div>
 
 <?php
     $a = new Area('Main');
